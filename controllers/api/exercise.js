2f3a7e4278f2de9c5776243c7789a00a6b883e52
Leo <leonardovrodriguez246@gmail.com>
2023-04-24 10:28:59 -0400

    working on controllers

---
+++ controllers/api/exercise.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const Plan = require('../../models/plan.js')
+
+module.exports = [
+    createExercise,
+    editExercise,
+    deleteExercise
+]
+
+
+async function createExercise (req, res){
+
+}
+
+async function editExercise (req, res){
+
+}
+
+async function deleteExercise (req, res){
+
+}
+
