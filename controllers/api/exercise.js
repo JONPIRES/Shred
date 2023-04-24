00d85b0ee2d344e5363ff08e4ffb3af9606e7379
Leo <leonardovrodriguez246@gmail.com>
2023-04-24 11:06:40 -0400

    added exericise controller(CRUD)

--- controllers/api/exercise.js
+++ controllers/api/exercise.js
@@ -1,23 +1,49 @@
 const express = require("express");
 const router = express.Router();
-const Plan = require('../../models/plan.js')
+const { Exercise } = require('../../models');
+const { getNextKeyDef } = require("@testing-library/user-event/dist/keyboard/getNextKeyDef");
 
-module.exports = [
+module.exports = {
     createExercise,
     editExercise,
-    deleteExercise
-]
+    deleteExercise,
+    viewExercise,
+}
 
+async function viewExercise(req, res) {
+    try {
+        const showExercise = await Exercise.findById(req.params.id)
+    } catch (error) {
+        console.log(error)
+        throw new Error ('View Exercise Error');
+    }
+}
 
-async function createExercise (req, res){
+async function createExercise(req, res) {
+    try {
+        await Exercise.create(req.body)
+    } catch (error) {
+        console.log(error)
+        throw new Error("Create Exercise Error")
+    }
 
 }
 
-async function editExercise (req, res){
-
+async function editExercise(req, res) {
+    try {
+       await Exercise.findByIdAndUpdate(req.body)
+    } catch (error) {
+        console.log(error)
+        throw new Error("Edit Exercise Error")
+    }
 }
 
-async function deleteExercise (req, res){
-
+async function deleteExercise(req, res) {
+try {
+    await Exercise.findByIdAndDelete(req.params.id) 
+} catch (error) {
+    console.log(error)
+    throw new Error("Delete Exercise Error")
+}
 }
 
--- routes/api/exercise.js
+++ routes/api/exercise.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const exerciseCtrl = require('../../controllers/api/exercise')
+
+router.get('/', exerciseCtrl.viewExercise)
+
+router.post('/create-exercise', exerciseCtrl.createExercise)
+
+router.put('/edit-exercise/:id', exerciseCtrl.editExercise)
+
+router.delete('/delete-exercise/:id', exerciseCtrl.deleteExercise)
