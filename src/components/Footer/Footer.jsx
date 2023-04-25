ad91de57338fda4abd439c06dfc6fa6d26c0ef0e
davidxv15 <davidxvaz@gmail.com>
2023-04-25 14:06:41 -0700

    Footer w/ add'l info

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,32 +10,33 @@ const Footer = () => {
         <div className="row">
           {/* {Column1} */}
           <div className="col">
-            <h5>SHRED</h5>
+            <h5>Trainers</h5>
             <ul className="list">
-              <li>2345</li>
-              <li>2345</li>
-              <li>2345</li>
+              <li>Pires</li>
+              <li>Rodriguez</li>
+              <li>Velasquez</li>
             </ul>
           </div>
           <div className="col">
-            <h5>SHRED</h5>
+            <h5>Follow</h5>
             <ul className="list">
-              <li>2345</li>
-              <li>2345</li>
-              <li>2345</li>
+              <li>FB</li>
+              <li>IG</li>
+              <li>TW</li>
             </ul>
           </div>
           <div className="col">
-            <h5>SHRED</h5>
+            <h5>Partners</h5>
             <ul className="list">
-              <li>2345</li>
-              <li>2345</li>
-              <li>2345</li>
+              <li>JEV Media</li>
+              <li>Trip City</li>
+              <li>Stack-Up</li>
+              <li>Lucy's</li>
             </ul>
           </div>
         </div>
       </div>
-<footer>Copyright © SHRED</footer>
+      <footer>Copyright © SHRED 2025</footer>
     </div>
   );
 };
