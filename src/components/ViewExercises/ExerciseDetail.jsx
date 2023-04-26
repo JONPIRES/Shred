1fe807467619afe40facba5100a42624d4180005
Leo <leonardovrodriguez246@gmail.com>
2023-04-26 00:45:35 -0400

    attempted Delete operation, fixed calling key/values from database

--- src/components/ViewExercises/ExerciseDetail.jsx
+++ src/components/ViewExercises/ExerciseDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import * as exercisesAPI from "../../utilities/exercises-api";
-
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 const ExerciseDetail = () => {
@@ -23,7 +23,26 @@ const ExerciseDetail = () => {
   }, []);
   console.log("this is the exercise: ", exercise);
 
-  return <div>{exercise.name}</div>;
+  return <div className="container" style={{minHeight:"100vh", height:'auto'}}>
+    <h1 className="text-center mt-3">{exercise.name}</h1>
+    <h3 className="text-center mt-2">Muscle Group: {exercise.muscleGroup}</h3>
+
+    <section className="row" >
+      <img src={exercise.img} alt="" className="col-lg-6 img-fluid" />
+      <div className="col-lg-6" style={{ marginTop: '5em' }}>
+        <p >Equipment: {exercise.equipment}</p>
+        <p>Explanation: {exercise.explanation}</p>
+      </div>
+    </section>
+    <div className="container text-center d-flex justify-content-center align-items-center">
+      <form method="POST"
+      action={`/exercise/${id}?_method=DELETE`}>
+        <button type="submit" className="btn btn-danger">DELETE</button>
+      </form>
+      <Link className="text-light link-underline-opacity-0 btn btn-primary " to='/update/${id}' style={{ marginTop: '1em', marginLeft:'2em'}}> UPDATE</Link>
+    </div>
+    </div>;
+
 };
 
 export default ExerciseDetail;
