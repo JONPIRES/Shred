2d83c1722513bbf4e5f0e1ddbeda7f8e101ea922
JONPIRES <jonbpires@gmail.com>
2023-04-28 10:49:04 -0300

    start displaying the user withing each exercise

--- src/components/ViewExercises/ExerciseCard.jsx
+++ src/components/ViewExercises/ExerciseCard.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import * as usersAPI from "../../utilities/users-api";
 
-const ExerciseCard = ({ img, name, muscle, equipment, id, user}) => {
+const ExerciseCard = ({ img, name, muscle, equipment, id, user }) => {
+  const [creator, setCreator] = useState();
+
+  useEffect(() => {
+    async function fetchCreator() {
+      const exUser = await usersAPI.get(user);
+      setCreator(exUser);
+    }
+    fetchCreator();
+  }, []);
+
   return (
     <div className="col-lg-4 col-md-6 mb-4">
-      <div className="card container container-fluid" style={{height:'35em'}}>
+      <div
+        className="card container container-fluid"
+        style={{ height: "35em" }}>
         <div className="text-center">
           <img
             src={img}
@@ -19,9 +32,9 @@ const ExerciseCard = ({ img, name, muscle, equipment, id, user}) => {
           <p className="card-text">Equipment: {equipment}</p>
           <p>{user}</p>
           <div className="text-center">
-          <Link to={`/exercise/${id}`} className="btn btn-primary p-2">
-            View Exercise
-          </Link>
+            <Link to={`/exercise/${id}`} className="btn btn-primary p-2">
+              View Exercise
+            </Link>
           </div>
         </div>
       </div>
